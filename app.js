aedfb2edf79a27f3bd3076fe04e07afa6b411da7
guilherme-gjv <gjv1@aluno.ifal.edu.br>
2022-06-07 13:10:32 -0300

    chore: account views

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 //images
 app.use(express.static("images"));
 
-/*mongoDB
+//mongoDB
 const mongoose = require("mongoose");
 mongoose
   .connect(process.env.DATABASE_URL, {
@@ -36,7 +36,6 @@ mongoose
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => console.log("Connected to MongoDB"));
-*/
 
 app.get("/", (req, res) => {
   res.redirect("/home");
@@ -58,7 +57,11 @@ app.get("/carrinho", (req, res) => {
   res.render("cart");
 });
 
-app.get("/meusPedidos", (req, res) => {
+app.get("/minhaConta", (req, res) => {
+  res.render("myAccount");
+});
+
+app.get("/minhaConta/meusPedidos", (req, res) => {
   res.render("myDemands");
 });
 
